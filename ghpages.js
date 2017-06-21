54677d97f790d3655c24ac996487b8f68c51bd72
Ben Janes <brjanes@gmail.com>
2017-06-21 11:53:45 -0400

    Try to pull before pushing to gh-pages

--- ghpages.js
+++ ghpages.js
@@ -1,38 +1,46 @@
 var fs = require('fs');
 var simpleGit = require('simple-git')();
 
-var commitHash;
-var file;
+var commitHash, file;
 
 // create a deploy dir in memory
 fs.readFile('./indexTpl.html', 'utf8', (err, data) => {
-	file = data;
+	const updateGHPages = writeToGHPages(commitHash, data);
+
 	simpleGit
-		// .branch((err,branches) => {
-		// 	commitHash = branches.branches.master.commit;
+		.branch((err,branches) => {
+			commitHash = branches.branches.master.commit;
 
-		// 	if (!~branches.all.indexOf('gh-pages')) {
-		// 		simpleGit.checkoutLocalBranch('gh-pages');
-		// 	} else {
-		// 		simpleGit.checkout('gh-pages');
-		// 	}
-		// })
-		.checkout('gh-pages', () => {
-			
-			fs.writeFile('./index.html', file, () => {
-				// console.log(file);
 
+			if (!~branches.all.indexOf('gh-pages')) {
+				simpleGit.checkoutLocalBranch('gh-pages', updateGHPages);
+			} else {
 				simpleGit
-					.add('.')
-					.commit('Redeploy for commit '/* + commitHash*/ + ' to master')
-					.push('origin', 'gh-pages', { '--no-verify': null });
-				
-			})
+					.checkout('gh-pages')
+					.pull('origin', 'gh-pages', updateGHPages);
+			}
 		})
+
+		// .checkout('gh-pages', () => {
+			
+		// })
 		// only if gh-pages exists in remote, pull it
 		// .pull('origin', 'gh-pages');
 
 })
+
+function writeToGHPages(commitHash, file) {
+	return () => {
+		fs.writeFile('./index.html', file, () => {
+
+			simpleGit
+				.add('.')
+				.commit('Redeploy for commit '/* + commitHash*/ + ' to master')
+				.push('origin', 'gh-pages', { '--no-verify': null });
+			
+		});
+	}
+}
 
 
 	// push to origin/gh-pages
