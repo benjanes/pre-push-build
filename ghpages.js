d104286a8386b4cdbfb9d8343045cfc494b5819a
Ben Janes <brjanes@gmail.com>
2017-06-21 10:52:34 -0400

    Add html file

--- ghpages.js
+++ ghpages.js
@@ -1,29 +1,36 @@
+var fs = require('fs');
 var simpleGit = require('simple-git')();
 
 var commitHash;
 
 // create a deploy dir in memory
+fs.readFile('./index.html', 'utf8', (err, data) => {
+	simpleGit
+		.branch((err,branches) => {
+			commitHash = branches.branches.master.commit;
 
-simpleGit
-	.branch((err,branches) => {
-		commitHash = branches.branches.master.commit;
+			if (!~branches.all.indexOf('gh-pages')) {
+				simpleGit.checkoutLocalBranch('gh-pages');
+			} else {
+				simpleGit.checkout('gh-pages');
+			}
+		})
+		// only if gh-pages exists in remote, pull it
+		.pull('origin', 'gh-pages', () => {
+			// delete files
+			// add files
+			fs.writeFile('./index.html', data, () => {
+				simpleGit
+					.add('.')
+					.commit('Redeploy for commit ' + commitHash + ' to master')
+					.push('origin', 'gh-pages', { '--no-verify': null });
+				
+			})
 
-		if (!~branches.all.indexOf('gh-pages')) {
-			simpleGit.checkoutLocalBranch('gh-pages');
-		} else {
-			simpleGit.checkout('gh-pages');
-		}
-	})
-	// only if gh-pages exists in remote, pull it
-	// .pull('origin', 'gh-pages', () => {
-	// 	// delete files
+		})
+})
 
-	// 	// add files
 
-	// })
-	.add('.')
-	.commit('Redeploy for commit ' + commitHash + ' to master')
-	.push('origin', 'gh-pages', { '--no-verify': null });
 	// push to origin/gh-pages
 	// checkout master
 	// if you don't do anything, does it exit with a push?
